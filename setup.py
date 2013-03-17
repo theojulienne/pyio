e9ae7558925e9d65f1f455f9527b4cb59b1cd734
Theo Julienne <theo@icy.com.au>
2013-03-17 14:07:39 +1100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='pyio',
+      version='0.1',
+      packages=find_packages(),
+      author='Theo Julienne',
+      author_email='theo.julienne+pyio@gmail',
+      license='MIT',
+      include_package_data=True,
+      description='pyio',
+      long_description='pyio',
+      platforms=['any'],
+      install_requires=[],
+)
